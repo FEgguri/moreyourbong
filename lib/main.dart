aada1e44f66f6f8da067379a5c05771617fe2d28
Kihwan-dev <rlrkf2420@gmail.com>
2025-08-07 14:58:55 +0900

    feat: chat page body ui 구성

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:moreyourbong/firebase_options.dart';
+import 'package:moreyourbong/views/pages/chat_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // debugPaintSizeEnabled = true;
   runApp((MyApp()));
 }
 
@@ -17,9 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Text('homeTest'),
-      ),
+      home: ChatPage(),
     );
   }
 }
--- lib/views/pages/chat_page.dart
+++ lib/views/pages/chat_page.dart
@@ -5,11 +5,115 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // String message = "안녕하세요";
+    String message = "안녕하세요 이 글은 장문의 글이라서 잘릴 수도 있어요 근데 제가 잘 설정 해놔서 4줄을 넘어가게 되면 ...으로 표시된답니다 ㅎ하하하하하하하하ㅏ하하하하하하하ㅏ하하하하하하하ㅏ";
+    String time = "오후 2:31";
+    int bottomSheetHeight = 100;
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        child: Text("ChatPage"),
+      backgroundColor: Color(0xFFFFCA28),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "모임 이름",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
+      // TODO 말풍선의 내용이 짧아지면 옆에 빈공간이 생김 어떻게 해결하지..?
+      body: ListView.separated(
+          padding: EdgeInsets.fromLTRB(12, 20, 12, bottomSheetHeight + 10),
+          itemCount: 10,
+          separatorBuilder: (context, index) {
+            return SizedBox(height: 10);
+          },
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 50,
+                    height: 50,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.network(
+                        "https://picsum.photos/200/300",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "오상구",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 4),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                            constraints: BoxConstraints(maxWidth: 250),
+                            decoration: BoxDecoration(
+                              color: Color(0xFFEEEEEE),
+                              borderRadius: BorderRadius.circular(14),
+                            ),
+                            child: Text(
+                              message,
+                              softWrap: true,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 4,
+                            ),
+                          ),
+                          SizedBox(width: 4),
+                          Text(
+                            time,
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey[600],
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            } else {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    time,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey[600],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(width: 4),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    constraints: BoxConstraints(maxWidth: 250),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFEEEEEE),
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                    child: Text(
+                      message,
+                      maxLines: 4,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              );
+            }
+          }),
       bottomSheet: SafeArea(
         child: Container(
           height: 100,
